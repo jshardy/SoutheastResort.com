35f1040c926efa17bc97f647c0c35887c77d31ce
Josh Hardy <jshardy@outlook.com>
2025-04-02 18:16:48 -0700

    Add a carousel and add about.

--- js/custom.js
+++ js/custom.js
@@ -42,19 +42,47 @@ window.onload = function () {
         });
     }
 
-    // $('.hero-carousel').slick({
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     autoplaySpeed: 4000,
-    //     arrows: false,
-    //     dots: false,
-    //     swipeToSlide: true,
-    //     useCSS: true,
-    //     useTransform: true,
-    //     lazyLoad: 'progressive',
-    //     centerMode: true,
-    //     centerPadding: '0px'
-    // });
+    if ($('.rooms-carousel').length) {
+        const carousel = $('#rooms-carousel');
+
+        const roomsCarousel = new bootstrap.Carousel(roomsCarouselId, {
+            interval: 3000,
+            touch: true
+        });
+    }
+
+    const hero = document.getElementById("hero");
+    const button = document.querySelector(".glowing-btn-wrapper");
+
+    window.addEventListener("scroll", () => {
+        const heroBottom = hero.getBoundingClientRect().bottom;
+        const halfWindowHeight = window.innerHeight / 2;
+
+        if (heroBottom <= halfWindowHeight) {
+            button.classList.add("show");
+        } else {
+            button.classList.remove("show");
+        }
+    });
+
+    let animated = false;
+
+    window.addEventListener("scroll", () => {
+        const scrollTop = window.scrollY;
+        const windowHeight = window.innerHeight;
+        const docHeight = document.documentElement.scrollHeight;
+
+        const scrolledToBottom = scrollTop + windowHeight >= docHeight - 10;
+
+        if (scrolledToBottom && !animated) {
+            button.classList.add("animate__hinge");
+            animated = true;
+        }
+
+        // Optional: reset animation when they scroll back up
+        if (!scrolledToBottom && animated) {
+            button.classList.remove("animate__hinge");
+            animated = false;
+        }
+    });
 };
